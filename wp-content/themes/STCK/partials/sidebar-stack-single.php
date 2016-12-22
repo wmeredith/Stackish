7ca4cbecce672514cbc2b3c2212847112f412cb5
Wade Meredith <wade@fire-engine-red.com>
2016-12-22 16:38:23 -0600

    Refined fontello icon fonts Built out single stack sidebar (very close to MVP)

--- wp-content/themes/STCK/partials/sidebar-stack-single.php
+++ wp-content/themes/STCK/partials/sidebar-stack-single.php
@@ -2,14 +2,14 @@
     <div class="row">
         <div class="col-md-12">
             <ul>
-                <li class="sidebar-section">
+                <li>
                     <h4>Typefaces</h4>
                     <ul>
                         <li>
-                            <a href="" title=""><span style="font-family: 'Helvetica', sans-serif">Helvetica</span></a>
+                            <a href="#" title="Go to Typeface"><span style="font-family: 'Helvetica', sans-serif"><h4>Helvetica</h4></span></a>
                         </li>
                         <li>
-                            <a href="" title=""><span style="font-family: 'Times New Roman', sans-serif">Times New Roman</span></a>
+                            <a href="#" title="Go to Typeface"><span style="font-family: 'Times New Roman', sans-serif"><h4>Times New Roman</h4></span></a>
                         </li>
                     </ul>
                 </li>
@@ -17,19 +17,20 @@
                     <h4>Spectrum</h4>
                     <ul>
                         <li>
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-sm" style="background: red;">Red</button>
-                                <button type="button" class="btn btn-default btn-sm" style="background: white;">White</button>
-                            </div>
+
+                        </li>
+                        <!--Might want to use one or either of these on these next two buttons-->
+                        <!--http://www.ajaxblender.com/howto-convert-image-to-grayscale-using-javascript.html-->
+                        <!--https://github.com/GianlucaGuarini/jQuery.BlackAndWhite-->
+                        <li>
+                            <a class="btn btn-default btn-sm" href="#" title="">
+                                <span aria-hidden="true" class="icon-tint"></span> Desaturate
+                            </a>
                         </li>
                         <li>
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-default btn-sm">Desaturate</button>
-                                <button type="button" class="btn btn-default btn-sm">Black &amp; White</button>
-                                <!--Might want to use one or either of these-->
-                                <!--http://www.ajaxblender.com/howto-convert-image-to-grayscale-using-javascript.html-->
-                                <!--https://github.com/GianlucaGuarini/jQuery.BlackAndWhite-->
-                            </div>
+                            <a class="btn btn-default btn-sm" href="#" title="">
+                                <span aria-hidden="true" class="icon-adjust"></span> B&amp;W
+                            </a>
                         </li>
                     </ul>
                 </li>
@@ -38,11 +39,84 @@
                     <ul>
                         <li>
                             <p>
-                                <a href="" title="">Tag</a>, <a href="" title="">Tag</a>, <a href="" title="">Tag</a>, <a href="" title="">Tag</a>, <a href="" title="">Tag</a>,
+                                <a href="#" title="">Tag</a>, <a href="#" title="">Tag</a>, <a href="#" title="">Tag</a>, <a href="#" title="">Tag</a>
                             </p>
                         </li>
                     </ul>
                 </li>
+                <li>
+                    <h4>Share</h4>
+                    <ul>
+                        <li>
+                            <div class="btn-group">
+                                <a class="btn btn-default btn-sm" href="" title="Send to Facebook">
+                                    <span aria-hidden="true" class="icon-facebook"></span>
+                                    <span class="sr-only">Facebook</span>
+                                </a>
+                                <a class="btn btn-default btn-sm" href="" title="">
+                                    <span aria-hidden="true" class="icon-twitter"></span>
+                                    <span class="sr-only">Twitter</span>
+                                </a>
+                                <a class="btn btn-default btn-sm" href="" title="">
+                                    <span aria-hidden="true" class="icon-pinterest"></span>
+                                    <span class="sr-only">Pinterest</span>
+                                </a>
+                                <a class="btn btn-default btn-sm" href="" title="">
+                                    <span aria-hidden="true" class="icon-dribbble"></span>
+                                    <span class="sr-only">Dribbble</span>
+                                </a>
+                                <a class="btn btn-default btn-sm" href="" title="">
+                                    <span aria-hidden="true" class="icon-mail"></span>
+                                    <span class="sr-only">Email</span>
+                                </a>
+                                <a class="btn btn-default btn-sm" href="" title="">
+                                    <span aria-hidden="true" class="icon-link"></span>
+                                    <span class="sr-only">Link</span>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <h4>Deploy</h4>
+                    <ul>
+                        <li>
+                            <a class="btn btn-default btn-sm" href="#" title="">
+                                <span aria-hidden="true" class="icon-doc-1"></span> Creative Brief (PDF)
+                            </a>
+                        </li>
+                        <li>
+                            <a class="btn btn-default btn-sm" href="#" title="Export as 300x200 PNG">
+                                <span aria-hidden="true" class="icon-picture"></span>
+                                300x200.png
+                            </a>
+                        </li>
+                        <li>
+                            <a class="btn btn-default btn-sm" href="#" title="Export as 1200x800 PNG"><!-- PRO Feature -->
+                                <span aria-hidden="true" class="icon-picture"></span>
+                                1200x800.png
+                            </a>
+                        </li>
+                        <li>
+                            <a class="btn btn-default btn-sm" href="#" title="Export as 1200x800 PNG"><!-- PRO Feature -->
+                                <span aria-hidden="true" class="icon-picture"></span>
+                                2400x1600.png
+                            </a>
+                        </li>
+
+                        <li>
+                            <a class="btn btn-default btn-sm" href="#" title="">
+                                <span aria-hidden="true" class="icon-docs"></span> Duplicate
+                            </a>
+                        </li>
+                        <li>
+                            <a class="btn btn-default btn-sm" href="#" title="Send to Facebook">
+                                <span aria-hidden="true" class="icon-code-1"></span> Raw
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
             </ul>
         </div>
     </div>
